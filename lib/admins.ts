5d563f3564fc161658bf5f90e9379bf6a160d3c7
Snehith Yamalapalli <snehithyjsdyamalapalli@gmail.com>
2026-06-17 23:22:53 +0530

    Added new gmails , firestore rules

--- lib/admins.ts
+++ lib/admins.ts
@@ -4,6 +4,9 @@ export const ADMIN_EMAILS = [
   "vadapallimohitvarma@gmail.com",
   "snehithyjsdyamalapalli@gmail.com",
   "lekhesh11@gmail.com",
+  "suryadevisrikota@gmail.com",
+  "matsapk05@gmail.com",
+  "swamyabhishekvelugotla.29@gmail.com",
 ];
 
 export function isAdminEmail(email: string | null | undefined): boolean {
